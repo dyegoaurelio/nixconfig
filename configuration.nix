ef18503d358df27257a2613219bb01f2bea37981
Dyego <dyegoaurelio@gmail.com>
2023-07-22 14:56:18 -0300

    kde connect working

--- configuration.nix
+++ configuration.nix
@@ -165,6 +165,9 @@
     pkgs.gnumake
   ];
 
+
+programs.kdeconnect.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
