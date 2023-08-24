2572fa8baca5100d6f37a3157b90ced893b6dbef
Dyego <dyegoaurelio@gmail.com>
2023-08-24 16:56:06 -0300

    use wayland and pipewire

--- configuration.nix
+++ configuration.nix
@@ -100,16 +100,16 @@
   services.printing.enable = true;
 
   # Enable sound with pipewire.
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
-  security.rtkit.enable = true;
+  hardware.pulseaudio.extraConfig = "load-module module-device-manager";
+
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
@@ -131,6 +131,7 @@
       gnomeExtensions.tactile
       gnomeExtensions.gsconnect
       gnomeExtensions.clipboard-indicator
+
     ];
   };
 
@@ -166,6 +167,8 @@
     pkgs.docker-compose
     pkgs.cmake
     pkgs.gnumake
+
+    pavucontrol
   ];
 
 
