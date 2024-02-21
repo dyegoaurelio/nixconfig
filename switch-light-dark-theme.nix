01ac6a43e9eb062d21de3fa0b59471a67d3ffc55
Dyego <dyegoaurelio@gmail.com>
2024-02-21 15:22:01 -0300

    make sure that themes are going to change

--- switch-light-dark-theme.nix
+++ switch-light-dark-theme.nix
@@ -1,27 +1,44 @@
 { config, pkgs, lib, ... }:
 
 let
-  # Script to switch to light theme
-  switchToLightThemeScript = pkgs.writeScript "switch-to-light-theme.sh" ''
+  switchThemeScript = pkgs.writeScript "switch-theme.sh" ''
     #!/bin/sh
-    # Command to switch to the light theme
-    ${pkgs.gsettings-desktop-schemas}/bin/gsettings set org.gnome.desktop.interface color-scheme default
+    currentHour=$(date +"%H")
+    
+    if [ $currentHour -ge 8 ] && [ $currentHour -lt 18 ]; then
+      # Between 08:00 and 18:00, switch to the light theme
+      ${pkgs.gsettings-desktop-schemas}/bin/gsettings set org.gnome.desktop.interface color-scheme default
+    else
+      # Otherwise, switch to the dark theme
+      ${pkgs.gsettings-desktop-schemas}/bin/gsettings set org.gnome.desktop.interface color-scheme prefer-dark
+    fi
   '';
 
-  # Script to switch to dark theme
-  switchToDarkThemeScript = pkgs.writeScript "switch-to-dark-theme.sh" ''
-    #!/bin/sh
-    # Command to switch to the dark theme
-    ${pkgs.gsettings-desktop-schemas}/bin/gsettings set org.gnome.desktop.interface color-scheme prefer-dark
-
-  '';
 in
 {
+  systemd.services.switch-theme = {
+    description = "Switch Theme on Boot";
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = switchThemeScript;
+    };
+    wantedBy = [ "multi-user.target" ];
+  };
+
+  systemd.timers.switch-theme = {
+    description = "Timer for Theme Switch";
+    wantedBy = [ "timers.target" ];
+    timerConfig = {
+      OnBootSec = "1min";  # Run the script 1 minute after boot
+    };
+  };
+
+
   systemd.services.switch-to-light-theme = {
     description = "Switch to Light Theme";
     serviceConfig = {
       Type = "oneshot";
-      ExecStart = switchToLightThemeScript;
+      ExecStart = switchThemeScript;
     };
   };
 
@@ -29,7 +46,7 @@ in
     description = "Switch to Dark Theme";
     serviceConfig = {
       Type = "oneshot";
-      ExecStart = switchToDarkThemeScript;
+      ExecStart = switchThemeScript;
     };
   };
 
@@ -37,7 +54,7 @@ in
     description = "Timer for Light Theme Switch";
     wantedBy = [ "timers.target" ];
     timerConfig = {
-      OnCalendar = "08:00";
+      OnCalendar = "08:01";
     };
   };
 
@@ -45,7 +62,7 @@ in
     description = "Timer for Dark Theme Switch";
     wantedBy = [ "timers.target" ];
     timerConfig = {
-      OnCalendar = "18:00";
+      OnCalendar = "18:01";
     };
   };
 
