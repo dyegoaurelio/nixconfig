c208862fa6c4138822ed1d3a587174c83e4b519e
Dyego Aurélio <dyegoaurelio@gmail.com>
2025-01-05 09:22:54 -0300

    delete nix-alien

--- configuration.example.nix
+++ configuration.example.nix
@@ -7,7 +7,6 @@
       ./hardware-configuration.nix
       ./manual-hardware-configuration.nix
       ./laptop-specifc-configuration.nix
-      ./nix-alien.nix
       ./switch-light-dark-theme.nix
       #./nix-encryption.nix
     ];
