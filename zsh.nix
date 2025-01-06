39708f05b9649cc4a30f1c3dfa7537949eb1033c
Dyego Aurélio <dyegoaurelio@gmail.com>
2025-01-05 21:47:37 -0300

    tweak zsh plugins

--- zsh.nix
+++ zsh.nix
@@ -3,21 +3,27 @@
   {
   environment.systemPackages = with pkgs; [
     thefuck # zsh plugin
-    zoxide # zsh plugin
+    zoxide # smarter cd
+    fzf # fuzzy finder used by zoxide
   ];
 
   users.defaultUserShell = pkgs.zsh;
   environment.shells = with pkgs; [ zsh ];
   programs.zsh = {
     enable = true;
-    enableCompletion = true;
-    autosuggestions.enable = true;
+    # enableCompletion = true;
+    enableCompletion = false;
+    # autosuggestions.enable = true;
     syntaxHighlighting.enable = true;
     histSize = 10000;
     ohMyZsh = {
       enable = true;
-      plugins = [ "git" "thefuck" "golang" "zoxide"];
+      plugins = [ "git" "thefuck" "golang" "zoxide" "yarn" ];
     };
+    interactiveShellInit = ''
+        source ${pkgs.zsh-nix-shell}/share/zsh-nix-shell/nix-shell.plugin.zsh
+        source ${pkgs.zsh-autocomplete}/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh
+      '';
   };
 
   imports = [
