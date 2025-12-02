6851470e0e756bad15c84a007ee12f92a9249e5c
Dyego Aurélio <dyegoaurelio@gmail.com>
2025-12-01 23:15:53 -0300

    use nvidia on desktop. also format desktop-specific-configuration.nix

--- desktop-specifc-configuration.nix
+++ desktop-specifc-configuration.nix
@@ -1,59 +1,56 @@
 { config, pkgs, ... }:
 
 {
-    networking.hostName = "nixos-desktop";
-    fileSystems."/data/m2_extra" =
-      { device = "/dev/disk/by-label/m2extra";
-        fsType = "ext4";
-      };
+  networking.hostName = "nixos-desktop";
+  fileSystems."/data/m2_extra" = {
+    device = "/dev/disk/by-label/m2extra";
+    fsType = "ext4";
+  };
 
   #   fileSystems."/data/ssd_extra" =
   # { device = "/dev/disk/by-label/Files";
   #   fsType = "ext4";
   # };
 
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
+  ## NVIDIA CONFIG
 
+  # Make sure opengl is enabled
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true;
+    extraPackages = with pkgs; [
+      vulkan-loader
+      vulkan-validation-layers
+      vulkan-extension-layer
+    ];
+  };
 
-## NVIDEA CONFIG
+  # Tell Xorg to use the nvidia driver (also valid for Wayland)
+  services.xserver.videoDrivers = [ "nvidia" ];
 
- # Make sure opengl is enabled
-# hardware.opengl = {
-#   enable = true;
-#   driSupport = true;
-#   driSupport32Bit = true;
-#   extraPackages = with pkgs; [
-#     vulkan-loader
-#     vulkan-validation-layers
-#     vulkan-extension-layer
-#   ];
-# };
+  hardware.nvidia = {
 
-#   # Tell Xorg to use the nvidia driver (also valid for Wayland)
-#   services.xserver.videoDrivers = ["nvidia"];
-  
-#   hardware.nvidia = {
+    #    forceFullCompositionPipeline = true;
 
-#    forceFullCompositionPipeline = true;
+    #Fix graphical corruption on suspend/resume
+    powerManagement.enable = true;
 
-# #Fix graphical corruption on suspend/resume
-#    powerManagement.enable = true;
+    # Modesetting is needed for most Wayland compositors
+    # (uncomment to use wayland)
+    modesetting.enable = true;
 
-#     # Modesetting is needed for most Wayland compositors
-# # (uncomment to use wayland)
-#     modesetting.enable = true;
+    # Use the open source version of the kernel module
+    # Only available on driver 515.43.04+
+    open = false;
 
-#     # Use the open source version of the kernel module
-#     # Only available on driver 515.43.04+
-#     open = false;
+    #     # Enable the nvidia settings menu
+    #     nvidiaSettings = true;
 
-#     # Enable the nvidia settings menu
-#     nvidiaSettings = true;
+    # Optionally, you may need to select the appropriate driver version for your specific GPU.
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  };
 
-#     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-#     package = config.boot.kernelPackages.nvidiaPackages.stable;
-
-#   };
-
-## END OF NVIDEA CONFIG
+  ## END OF NVIDIA CONFIG
 }
