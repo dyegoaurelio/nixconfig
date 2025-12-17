9a2ec0241144997bca382c9321bb7d8bc7591b46
Dyego Aurélio <dyegoaurelio@gmail.com>
2025-12-16 22:50:08 -0300

    rm nvidia

--- desktop-specifc-configuration.nix
+++ desktop-specifc-configuration.nix
@@ -17,40 +17,38 @@
   ## NVIDIA CONFIG
 
   # Make sure opengl is enabled
-  hardware.graphics = {
-    enable = true;
-    enable32Bit = true;
-    extraPackages = with pkgs; [
-      vulkan-loader
-      vulkan-validation-layers
-      vulkan-extension-layer
-    ];
-  };
+  # hardware.graphics = {
+  #   enable = true;
+  #   enable32Bit = true;
+  #   extraPackages = with pkgs; [
+  #     vulkan-loader
+  #     vulkan-validation-layers
+  #     vulkan-extension-layer
+  #   ];
+  # };
 
   # Tell Xorg to use the nvidia driver (also valid for Wayland)
-  services.xserver.videoDrivers = [ "nvidia" ];
+  # services.xserver.videoDrivers = [ "nvidia" ];
 
-  hardware.nvidia = {
-
+  # hardware.nvidia = {
     #    forceFullCompositionPipeline = true;
-
     #Fix graphical corruption on suspend/resume
-    powerManagement.enable = true;
+    # powerManagement.enable = true;
 
     # Modesetting is needed for most Wayland compositors
     # (uncomment to use wayland)
-    modesetting.enable = true;
+    # modesetting.enable = true;
 
     # Use the open source version of the kernel module
     # Only available on driver 515.43.04+
-    open = false;
+    # open = false;
 
     #     # Enable the nvidia settings menu
     #     nvidiaSettings = true;
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
+    # package = config.boot.kernelPackages.nvidiaPackages.stable;
+  # };
 
   ## END OF NVIDIA CONFIG
 }
