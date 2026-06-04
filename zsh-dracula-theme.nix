64047e8659533bde4f38099b0b3555e18d10c633
Dyego Aurélio <dyegoaurelio@gmail.com>
2026-06-04 18:45:16 -0300

    bump zsh Dracula theme

--- zsh-dracula-theme.nix
+++ zsh-dracula-theme.nix
@@ -3,8 +3,8 @@ let
   dracula-theme = pkgs.fetchFromGitHub {
     owner = "dracula";
     repo = "zsh";
-    rev = "75ea3f5e1055291caf56b4aea6a5d58d00541c41";
-    sha256 = "sha256-TuKC1wPdq2OtEeViwnAmitpdaanyXHJmBcqV+rHxy34=";
+    rev = "a3e27d47ea2ed1e3b435f44aa71caf71d3219af6";
+    sha256 = "sha256-unPUH3D89gH0j8/kv1Dl+ybR5n8UX0hJ+SuETtgpJOo=";
   };
 
   zsh-customs = pkgs.stdenv.mkDerivation {
