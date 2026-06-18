a31e77fb79fcdab6e62ce6373b231cd4905bf2b1
Dyego Aurélio <dyegoaurelio@gmail.com>
2026-06-18 13:30:33 -0300

    delete nvidia config it was already commented out. just removing noise

--- desktop-specifc-configuration.nix
+++ desktop-specifc-configuration.nix
@@ -15,41 +15,4 @@
   boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.kernelModules = [ "ntsync" ];
 
-  ## NVIDIA CONFIG
-
-  # Make sure opengl is enabled
-  # hardware.graphics = {
-  #   enable = true;
-  #   enable32Bit = true;
-  #   extraPackages = with pkgs; [
-  #     vulkan-loader
-  #     vulkan-validation-layers
-  #     vulkan-extension-layer
-  #   ];
-  # };
-
-  # Tell Xorg to use the nvidia driver (also valid for Wayland)
-  # services.xserver.videoDrivers = [ "nvidia" ];
-
-  # hardware.nvidia = {
-    #    forceFullCompositionPipeline = true;
-    #Fix graphical corruption on suspend/resume
-    # powerManagement.enable = true;
-
-    # Modesetting is needed for most Wayland compositors
-    # (uncomment to use wayland)
-    # modesetting.enable = true;
-
-    # Use the open source version of the kernel module
-    # Only available on driver 515.43.04+
-    # open = false;
-
-    #     # Enable the nvidia settings menu
-    #     nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    # package = config.boot.kernelPackages.nvidiaPackages.stable;
-  # };
-
-  ## END OF NVIDIA CONFIG
 }
